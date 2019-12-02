a786900fd12985c927482fe2c82fc1304dfc66f3
lucasmantoani <lucas.mantoani22@gmail.com>
2019-12-02 21:11:45 +0100

    year error + fixes

---
+++ Controle/target/debug/deps/Controle-c02fdbd89ada1ee8.d
@@ -0,0 +1,5 @@
+/home/lucas/Git/Rust_practice/Controle/target/debug/deps/libControle-c02fdbd89ada1ee8.rlib: src/lib.rs
+
+/home/lucas/Git/Rust_practice/Controle/target/debug/deps/Controle-c02fdbd89ada1ee8.d: src/lib.rs
+
+src/lib.rs:
---
+++ Controle/target/debug/libControle.d
@@ -0,0 +1,1 @@
+/home/lucas/Git/Rust_practice/Controle/target/debug/libControle.rlib: /home/lucas/Git/Rust_practice/Controle/src/lib.rs
